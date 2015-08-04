6c4d10222d524698a77d011c27e669778f41c22f
charmainebonifacio <charmaine.bonifacio@gmail.com>
2015-08-04 17:10:27 -0600

    Update Summary
    Update modules with additional comments for each subroutine. Revised
    July 30, 2015.

--- M_SYSTEMCHECK.f90
+++ M_SYSTEMCHECK.f90
@@ -3,22 +3,27 @@
 !-------------------------------------------------------------------
 ! CREATED BY   : Charmaine Bonifacio
 ! DATE CREATED : July 24, 2015
-! DATE REVISED : July 28, 2015
+! DATE REVISED : July 30, 2015
 !-------------------------------------------------------------------
-! DESCRIPTION  : The module will contain DATE and TIME subroutines.
-! SUBROUTINE 1 : This subroutine will calculate the date and time.
-! SUBROUTINE 2 : This subroutine will calculate the elapsed time.
-! SUBROUTINE 3 : This subroutine will check if the file was opened
-!                successfully.
-! SUBROUTINE 4 : This subroutine will check ISUBNO value.
+! DESCRIPTION  : The module will be used to check the date and time.
 !###################################################################
 module m_systemcheck
 
-    use m_systemlog, only: programHeader
     implicit none
 
 contains
 
+!-------------------------------------------------------------------------------
+!
+!  SUBROUTINE TITLE  :  DATETIMELOG
+!       DESCRIPTION  :  This subroutine will calculate the date and time.
+!       AUTHORED BY  :  Charmaine Bonifacio
+!      DATE REVISED  :  July 30, 2015
+!        PARAMETERS  :  Character, OUTPUT, date of the run (YYYY_MM_DD format)
+!                       Character, OUTPUT, date of the run (YYYY-MM-DD format)
+!                       Character, OUTPUT, time of the run
+!
+!-------------------------------------------------------------------------------
    subroutine datetimelog(date, datenow, timenow)
 
        character(len=8) :: dateinfo
@@ -40,6 +45,18 @@ contains
 
    end subroutine datetimelog
 
+!-------------------------------------------------------------------------------
+!
+!  SUBROUTINE TITLE  :  ELAPSEDTIME
+!       DESCRIPTION  :  This subroutine will calculate the elapsed time.
+!       AUTHORED BY  :  Charmaine Bonifacio
+!      DATE REVISED  :  July 30, 2015
+!        PARAMETERS  :  Integer, OUTPUT, total time the program ran
+!                       Integer, INPUT, start count
+!                       Integer, INPUT, end count
+!                       Integer, INPUT, rate count
+!
+!-------------------------------------------------------------------------------
    subroutine elapsedtime(elapsed_time, sys_count_0, sys_count_1, countrate)
 
        integer, intent(in) :: sys_count_0, sys_count_1, countrate
@@ -48,35 +65,5 @@ contains
        elapsed_time = real(sys_count_1 - sys_count_0)/ real(countrate)
 
    end subroutine elapsedtime
-
-   subroutine filestatcheck(status, unit_no)
-
-       integer, intent(in) :: unit_no, status
-
-       if (status==0) then
-           write(unit_no,*) 'SUCCESSFULLY OPENED FILE.'
-       end if
-       if (status/=0) then
-           write(unit_no,*) 'COULD NOT OPEN FILE.'
-       end if
-
-   end subroutine filestatcheck
-
-    subroutine valuecheck(value, unit_no1, unit_no2)
-
-        integer, intent(in) :: unit_no1, unit_no2, value
-
-        if (value==0) then
-            write(unit_no1,*)
-            write(unit_no1,*) programHeader
-            write(unit_no1,*)
-            write(unit_no1,*) 'INVALID VALUE. TERMINATING PROGRAM'
-            write(unit_no1,*)
-            close(unit_no1)
-            close(unit_no2)
-            stop ' INVALID VALUE. TERMINATING PROGRAM. '
-        end if
-
-    end subroutine valuecheck
 
 end module m_systemcheck
--- M_SYSTEMLOG.f90
+++ M_SYSTEMLOG.f90
@@ -3,20 +3,17 @@
 !-------------------------------------------------------------------
 ! CREATED BY   : Charmaine Bonifacio
 ! DATE CREATED : July 27, 2015
-! DATE REVISED : July 28, 2015
+! DATE REVISED : July 30, 2015
 !-------------------------------------------------------------------
 ! DESCRIPTION  : The module will contain various subroutines
 !                needed to format the LOG FILE.
-! SUBROUTINE 1 : This subroutine will print out the start log
-!                header for this script.
-! SUBROUTINE 2 : This subroutine will print out the end log
-!                header for this script.
 !###################################################################
 module m_systemlog
 
     implicit none
     character(11), parameter :: debugStat = '[ STATUS ] '
     character(11), parameter :: debugRes = '[ RESULT ] '
+    character(11), parameter :: debugLog = '[ LOGGED ] '
     character(67), parameter :: programHeader = "###################################################################"
     character(20), parameter :: dayStat = '             DATE : '
     character(20), parameter :: timeStat = '             TIME : '
@@ -25,10 +22,21 @@ module m_systemlog
     character(20), parameter :: fileNameOpened =  '  FILENAME OPENED : '
     character(20), parameter :: fileStat =  '      FILE STATUS : '
     character(81), parameter :: lineHeader = '================================================================================='
+    character(len=*), parameter:: format_status_line = '( 1X, A11, A26 )'
     save
 
 contains
 
+!-------------------------------------------------------------------------------
+!
+!  SUBROUTINE TITLE  :  STARTPROGRAMLOG
+!       DESCRIPTION  :  This subroutine will print out the start log
+!                       header for this script.
+!       AUTHORED BY  :  Charmaine Bonifacio
+!      DATE REVISED  :  July 30, 2015
+!        PARAMETERS  :  Integer, INPUT, unit number associated with file opened
+!
+!-------------------------------------------------------------------------------
     subroutine startprogramlog(unit_no)
 
         integer, intent(in) :: unit_no
@@ -45,6 +53,16 @@ contains
 
     end subroutine startprogramlog
 
+!-------------------------------------------------------------------------------
+!
+!  SUBROUTINE TITLE  :  ENDPROGRAMLOG
+!       DESCRIPTION  :  This subroutine will print out the end log
+!                       header for this script.
+!       AUTHORED BY  :  Charmaine Bonifacio
+!      DATE REVISED  :  July 30, 2015
+!        PARAMETERS  :  Integer, INPUT, unit number associated with file opened
+!
+!-------------------------------------------------------------------------------
     subroutine endprogramlog(unit_no)
 
         integer, intent(in) :: unit_no
@@ -58,5 +76,58 @@ contains
         write(unit_no,*)
         write(unit_no,*) 'END OF PROGRAM. '
      end subroutine endprogramlog
+
+!-------------------------------------------------------------------------------
+!
+!  SUBROUTINE TITLE  :  FILESTATCHECK
+!       DESCRIPTION  :  This subroutine will check if the file was opened
+!                       successfully.
+!       AUTHORED BY  :  Charmaine Bonifacio
+!      DATE REVISED  :  July 30, 2015
+!        PARAMETERS  :  Integer, INPUT, unit number associated with file opened
+!                       Integer, INPUT, status value 0 is successful
+!
+!-------------------------------------------------------------------------------
+   subroutine filestatcheck(status, unit_no)
+
+       integer, intent(in) :: unit_no, status
+
+       if (status==0) then
+           write(unit_no,format_status_line) debugStat, ' SUCCESSFULLY OPENED FILE.'
+       end if
+       if (status/=0) then
+           write(unit_no,format_status_line) debugStat, ' COULD NOT OPEN FILE.'
+       end if
+
+   end subroutine filestatcheck
+
+!-------------------------------------------------------------------------------
+!
+!  SUBROUTINE TITLE  :  VALUECHECK
+!       DESCRIPTION  :  This subroutine will check the validity of the
+!                       ISUBNO value before proceeding with the calibration.
+!       AUTHORED BY  :  Charmaine Bonifacio
+!      DATE REVISED  :  July 30, 2015
+!        PARAMETERS  :  Integer, INPUT, value 0 means no ISUBNO found
+!                    :  Integer, INPUT, unit number associated with file opened
+!                    :  Integer, INPUT, unit number associated with file opened
+!
+!-------------------------------------------------------------------------------
+    subroutine valuecheck(value, unit_no1, unit_no2)
+
+        integer, intent(in) :: unit_no1, unit_no2, value
+
+        if (value==0) then
+            write(unit_no1,*)
+            write(unit_no1,*) programHeader
+            write(unit_no1,*)
+            write(unit_no1,*) 'INVALID VALUE. TERMINATING PROGRAM'
+            write(unit_no1,*)
+            close(unit_no1)
+            close(unit_no2)
+            stop ' INVALID VALUE. TERMINATING PROGRAM. '
+        end if
+
+    end subroutine valuecheck
 
 end module m_systemlog
