58cf274420425510699070b52bdbe8687d7bb17b
charmainebonifacio <charmaine.bonifacio@gmail.com>
2015-08-04 17:37:38 -0600

    Update Summary
    Updated log program that utilizes character for formatting output text.
    Revised July 31, 2015.

--- ACRU_MENU_LOG.f90
+++ ACRU_MENU_LOG.f90
@@ -2,11 +2,13 @@
 ! TITLE        : ACRU_MENU_LOG
 !-------------------------------------------------------------------
 ! CREATED BY   : CHARMAINE BONIFACIO
-! DATE REVISED : JULY 30, 2015
+! DATE CREATED : NOVEMBER 25, 2014
+! DATE REVISED : JULY 31, 2015
 !-------------------------------------------------------------------
 ! DESCRIPTION  : THIS IS A TEST RUN FOR IMPLEMENTING THE LOGFILE
 !                SYSTEM.
 ! REQUIREMENT  : MUST RUN THE .EXE FILE WITHIN THE INPUT DIRECTORY.
+! MODULES      : MUST INCLUDE M_SYSTEMCHECK AND M_SYSTEMLOG
 ! INPUT        : 1) MENU FILE = MENU
 !                2) VARIABLE FILE = MENU_VARIABLE.TXT
 ! OUTPUT       : 1) LOG FILE
@@ -17,83 +19,81 @@ use m_systemcheck
 use m_systemlog
 implicit none
 
-character(len=10), parameter :: debugstat = '[ status ] '
-character(len=10), parameter :: debugres = '[ result ] '
-character(len=10), parameter :: debugask = '[  ask  ] '
-integer :: line,i,j,k,l,m,n,o,p,ok,iocheck
-integer :: isubno
+character(len=4), parameter :: menu = 'MENU'
+character(len=*), parameter :: menuvars = 'menu_variable.txt'
+character(len=*), parameter:: format_etime = '(1X, A11, A20, F10.5)'
+character(len=*), parameter:: format_logfile = '(1X, A11, A20, A30)'
+character(len=*), parameter:: format_logstat = '(1X, A11, A20, A20)'
+character(len=*), parameter:: format_daytime = '(1X, A11, A20, A15)'
+character(len=*), parameter:: format_filestat = '(1X, A11, A20, I4)'
+character(len=*), parameter:: format_endmsg = '( A75,A10,A3,A5 )'
+character(len=30) :: outfile, infile, logrun, varfile
+character(len=80) :: dum, dum2, msg
+character(len=10) :: date, date_now, date_end
+character(len=12) :: time_now, time_end
 integer :: count_0, count_1, count_rate, count_max
-character(len=4), parameter :: menu = 'menu'
-character(len=50), parameter :: menuvars = "menu_variable.txt"
-character(len=200) :: outfile, infile, varfile, tmpfile, logrun
-character(len=200) :: menucopy, menutmp, dir, copyfile, killfile
-character(len=80) :: dummy
-character(len=8) :: dateinfo
-character(len=4) :: year, month*2, day*2
-character(len=2) :: hrs, min, sec*6
-character(len=10) :: date, timeinfo, timenow*12, datenow, timeend*12, dateend
+integer :: i, p, ok
 logical :: ex
+real :: elapsed_time
 
 !***********************************************************************
-! SETUP PROGRAM TIME
-!***********************************************************************
-      call system_clock(count_0, count_rate, count_max)
-      call datetimelog(date, datenow, timenow)
-!***********************************************************************
-! START PROGRAM
-!***********************************************************************
-      call startprogramlog(12)
-      write(*,*) debugstat, ' date of log  -> ', datenow
-      write(*,*) debugstat, ' time of log  -> ', timenow
-      write(*,*)
-      logrun = 'logrun_menu_'//date//'.txt'
-      inquire(file=logrun, exist=ex)
-      write(*,*) debugstat, ' checking file: ', logrun
-      if (ex) then
+! START PROGRAM - DAY & TIME SETUP AND LOGFILE SETUP
+    call system_clock(count_0, count_rate, count_max)
+    call datetimelog(date, date_now, time_now)
+    logrun = 'LOGRUN_MENU_'//date//'.txt'
+    inquire(file=logrun, exist=ex)
+    write(*,*) debugStat, ' checking file: ', logrun
+    if (ex) then
         open(unit=12,file=logrun,status='replace',iostat=ok)
-      else
+    else
         open(unit=12,file=logrun,status='new',iostat=ok)
-      endif
-!     if (ok/=0) then
-!       write(*,*) debugres, 'could not open file.'
-!       stop
-!     endif
-      write(*,*) debugres, ' file opened: ', logrun
-      write(*,*) debugres, ' file status ok = ', ok
-      write(12,*)
+    endif
+    call startprogramlog(12)
+    write(12,format_daytime) debugLog, dayStat, date_now
+    write(12,format_daytime) debugLog, timeStat, time_now
+    write(12,*)
+    write(12,format_logfile) debugLog, logfileStat, logrun
+    write(12,format_filestat) debugLog, fileStat, ok
+    write(12,*)
+    write(12,*) '[ C R E A T I N G   M E N U   F I L E ] '
+    write(12,*)
+    infile = menu
+    outfile = menu//'_OLD'
+    call system( "copy " // infile // " " // outfile)
+    call system( "copy " // infile // " " // 'ORIGINAL_'//menu)
+    write(12,*) debugStat, ' COPIED MENU FILE AND RENAMED TO MENU_OLD. '
+    varfile = menuvars
+    open(unit=11,file=varfile,iostat=ok)
+    write(12,*) '>> PROCESSING VARIABLE FILE...'
+    call filestatcheck(ok,12)
+    write(12,format_logstat) debugStat, fileNameOpened, varfile
+    write(12,format_filestat) debugStat, fileStat, ok
+    write(12,*)
+    open(unit=20,file=outfile,iostat=ok)
+    write(12,*) '>> PROCESSING MENU_OLD COPY OF MENU FILE.'
+    call filestatcheck(ok,12)
+    write(12,format_logstat) debugStat, fileNameOpened, outfile
+    write(12,format_filestat) debugStat, fileStat, ok
+    write(12,*)
+    open(unit=30,file=infile,iostat=ok)
+    write(12,*) '>> PROCESSING WORKING COPY OF MENU FILE.'
+    call filestatcheck(ok,12)
+    write(12,format_logstat) debugStat, fileNameOpened, infile
+    write(12,format_filestat) debugStat, fileStat, ok
+    write(12,*)
+    msg = ' MENU CALIBRATED & CREATED BY CHARMAINE BONIFACIO. MENU SCRIPT VERSION --- '
+    write(30,format_endmsg) msg, date, ' | ', time_now
+    endfile(30)
+    close(30)
 !***********************************************************************
-! START LOG
-!***********************************************************************
-      write(12,*) debugstat, ' date -> ', datenow
-      write(12,*) debugstat, ' time -> ', timenow
-      write(12,*)
-      write(12,*) debugstat, ' logfile -> ', logrun
-      write(12,*) debugstat, ' status -> ', ok
-      varfile = menuvars
-      open(unit=10,file=varfile,iostat=ok)
-      write(12,*) debugstat, ' variable file -> ', varfile
-      write(12,*) debugstat, ' status -> ', ok
-      outfile = menu
-      open(unit=20,file=outfile,iostat=ok)
-      write(12,*) debugstat, ' menufile -> ', outfile
-      write(12,*) debugstat, ' status -> ', ok
-      menucopy = menu//'_old'
-      call system( "copy " // outfile // " " // menucopy)
-      open(unit=30,file=menucopy,iostat=ok)
-      write(12,*) debugstat, ' menufile copy -> ', menucopy
-      write(12,*) debugstat, ' status -> ', ok
-      close(10)
-  999 close(30)
-      close(20)
-!***********************************************************************
-! ELAPSED TIME
-!***********************************************************************
-      call system_clock(count_1, count_rate, count_max)
-      call datetimelog(date, dateend, timeend)
-!***********************************************************************
-! END PROGRAM
-!***********************************************************************
-      call endprogramlog(12)
-      close(12)
-   	  stop
+! END PROGRAM - ELAPSED TIME
+    call system_clock(count_1, count_rate, count_max)
+    call datetimelog(date, date_end, time_end)
+    write(12,format_daytime) debugStat, dayStat, date_end
+    write(12,format_daytime) debugStat, timeStat, time_end
+    call elapsedtime(elapsed_time, count_0, count_1, count_rate)
+    write(12,format_etime) debugStat, etimeStat, elapsed_time
+    call endprogramlog(12)
+    close(12)
+
 end program acru_menu_log
