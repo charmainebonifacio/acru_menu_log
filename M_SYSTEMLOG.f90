9d18efe34202903daf974718183a5a41f7064964
charmainebonifacio <charmaine.bonifacio@gmail.com>
2015-08-07 15:35:30 -0600

    Update Summary
    Updated module format to save space. Revised August 7, 2015.

--- M_SYSTEMLOG.f90
+++ M_SYSTEMLOG.f90
@@ -1,40 +1,64 @@
-!###################################################################
+!###############################################################################
 ! MODULE TITLE : M_SYSTEMLOG
-!-------------------------------------------------------------------
-! CREATED BY   : Charmaine Bonifacio
-! DATE CREATED : July 27, 2015
-! DATE REVISED : July 30, 2015
-!-------------------------------------------------------------------
-! DESCRIPTION  : The module will contain various subroutines
-!                needed to format the LOG FILE.
-!###################################################################
+! CREATED BY   : CHARMAINE BONIFACIO
+! DATE CREATED : JULY 27, 2015
+! DATE REVISED : AUGUST 7, 2015
+! DESCRIPTION  : THE MODULE CONTAINS SUBROUTINES NEEDED TO FORMAT THE LOG FILE.
+!###############################################################################
 module m_systemlog
 
     implicit none
     character(11), parameter :: debugStat = '[ STATUS ] '
     character(11), parameter :: debugRes = '[ RESULT ] '
     character(11), parameter :: debugLog = '[ LOGGED ] '
-    character(67), parameter :: programHeader = "###################################################################"
+    character(len=*), parameter :: logHeader =  '*******************************************************************'
+    character(67), parameter :: programHeader = '###################################################################'
     character(20), parameter :: dayStat = '             DATE : '
     character(20), parameter :: timeStat = '             TIME : '
     character(20), parameter :: etimeStat = '     ELAPSED TIME : '
     character(20), parameter :: logfileStat = '          LOGFILE : '
     character(20), parameter :: fileNameOpened =  '  FILENAME OPENED : '
     character(20), parameter :: fileStat =  '      FILE STATUS : '
-    character(81), parameter :: lineHeader = '================================================================================='
-    character(len=*), parameter:: format_status_line = '( 1X, A11, A26 )'
+    character(81), parameter :: sectionHeader = '================================================================================='
+    character(len=*), parameter:: format_status_line = '( 1X,A11,A26 )'
     save
 
 contains
 
 !-------------------------------------------------------------------------------
 !
+!  SUBROUTINE TITLE  :  HEADERLOG
+!       DESCRIPTION  :  THIS SUBROUTINE WILL PRINT OUT THE HEADER CONTAINING
+!                       SPECIFIC INFORMATION.
+!       AUTHORED BY  :  CHARMAINE BONIFACIO
+!      DATE REVISED  :  AUGUST 1, 2015
+!        PARAMETERS  :  INTEGER, INPUT, UNIT NUMBER ASSOCIATED WITH FILE OPENED
+!
+!-------------------------------------------------------------------------------
+    subroutine headerlog(unit_no)
+
+        integer, intent(in) :: unit_no
+
+        write(unit_no,*)
+        write(unit_no,*) logHeader
+        write(unit_no,*)
+        write(unit_no,*) '                          ACRU MENU LOG FILE'
+        write(unit_no,*)
+        write(unit_no,*) ' CREATED BY   : Charmaine Bonifacio'
+        write(unit_no,*) ' DATE REVISED : August 6, 2015'
+        write(unit_no,*)
+        write(unit_no,*) logHeader
+
+    end subroutine headerlog
+
+!-------------------------------------------------------------------------------
+!
 !  SUBROUTINE TITLE  :  STARTPROGRAMLOG
-!       DESCRIPTION  :  This subroutine will print out the start log
-!                       header for this script.
-!       AUTHORED BY  :  Charmaine Bonifacio
-!      DATE REVISED  :  July 30, 2015
-!        PARAMETERS  :  Integer, INPUT, unit number associated with file opened
+!       DESCRIPTION  :  THIS SUBROUTINE WILL PRINT OUT THE START LOG
+!                       HEADER FOR THIS SCRIPT.
+!       AUTHORED BY  :  CHARMAINE BONIFACIO
+!      DATE REVISED  :  JULY 30, 2015
+!        PARAMETERS  :  INTEGER, INPUT, UNIT NUMBER ASSOCIATED WITH FILE OPENED
 !
 !-------------------------------------------------------------------------------
     subroutine startprogramlog(unit_no)
@@ -56,11 +80,11 @@ contains
 !-------------------------------------------------------------------------------
 !
 !  SUBROUTINE TITLE  :  ENDPROGRAMLOG
-!       DESCRIPTION  :  This subroutine will print out the end log
-!                       header for this script.
-!       AUTHORED BY  :  Charmaine Bonifacio
-!      DATE REVISED  :  July 30, 2015
-!        PARAMETERS  :  Integer, INPUT, unit number associated with file opened
+!       DESCRIPTION  :  THIS SUBROUTINE WILL PRINT OUT THE END LOG
+!                       HEADER FOR THIS SCRIPT.
+!       AUTHORED BY  :  CHARMAINE BONIFACIO
+!      DATE REVISED  :  JULY 30, 2015
+!        PARAMETERS  :  INTEGER, INPUT, UNIT NUMBER ASSOCIATED WITH FILE OPENED
 !
 !-------------------------------------------------------------------------------
     subroutine endprogramlog(unit_no)
@@ -80,12 +104,12 @@ contains
 !-------------------------------------------------------------------------------
 !
 !  SUBROUTINE TITLE  :  FILESTATCHECK
-!       DESCRIPTION  :  This subroutine will check if the file was opened
-!                       successfully.
-!       AUTHORED BY  :  Charmaine Bonifacio
-!      DATE REVISED  :  July 30, 2015
-!        PARAMETERS  :  Integer, INPUT, unit number associated with file opened
-!                       Integer, INPUT, status value 0 is successful
+!       DESCRIPTION  :  THIS SUBROUTINE WILL CHECK IF THE FILE WAS OPENED
+!                       SUCCESSFULLY.
+!       AUTHORED BY  :  CHARMAINE BONIFACIO
+!      DATE REVISED  :  JULY 30, 2015
+!        PARAMETERS  :  INTEGER, INPUT, UNIT NUMBER ASSOCIATED WITH FILE OPENED
+!                       INTEGER, INPUT, STATUS VALUE 0 IS SUCCESSFUL
 !
 !-------------------------------------------------------------------------------
    subroutine filestatcheck(status, unit_no)
@@ -104,13 +128,13 @@ contains
 !-------------------------------------------------------------------------------
 !
 !  SUBROUTINE TITLE  :  VALUECHECK
-!       DESCRIPTION  :  This subroutine will check the validity of the
-!                       ISUBNO value before proceeding with the calibration.
-!       AUTHORED BY  :  Charmaine Bonifacio
-!      DATE REVISED  :  July 30, 2015
-!        PARAMETERS  :  Integer, INPUT, value 0 means no ISUBNO found
-!                    :  Integer, INPUT, unit number associated with file opened
-!                    :  Integer, INPUT, unit number associated with file opened
+!       DESCRIPTION  :  THIS SUBROUTINE WILL CHECK THE VALIDITY OF THE
+!                       ISUBNO VALUE BEFORE PROCEEDING WITH THE CALIBRATION.
+!       AUTHORED BY  :  CHARMAINE BONIFACIO
+!      DATE REVISED  :  JULY 30, 2015
+!        PARAMETERS  :  INTEGER, INPUT, VALUE 0 MEANS NO ISUBNO FOUND
+!                    :  INTEGER, INPUT, UNIT NUMBER ASSOCIATED WITH FILE OPENED
+!                    :  INTEGER, INPUT, UNIT NUMBER ASSOCIATED WITH FILE OPENED
 !
 !-------------------------------------------------------------------------------
     subroutine valuecheck(value, unit_no1, unit_no2)
